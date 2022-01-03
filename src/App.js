a0b26ab5e1cfc0a8c6dd1febc8985cc13d520b01
fadilkun45 <indocrafter76@gmail.com>
2022-01-03 20:57:47 +0700

    update 2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import { nanoid } from 'nanoid'
 import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
+import Send from './Send'
 
 const App = () => {
+    const socket = io.connect('http://localhost:3001',{
+    })
+
     let [roomName,setRoomName] = useState()
     let [roomStat,setRoomStat ] = useState()
-    let [listRoom,setListRoom] = useState([{'ruangan': '1212', "id" : '131313','player': '1/2'},{'ruangan': '31313', "id" : '131313','player': '1/2'}])
+    let [listRoom,setListRoom] = useState([])
 
     let createRoom = () => {
-        setListRoom([...listRoom, {'ruangan' : roomName,"id": nanoid(10) ,'player': '2/2'}])
-        console.log(listRoom)
         setRoomName('')
+        socket.emit('createRoom',{roomType: "public",roomName: roomName,players: "tes"})
+     
     }
 
     useEffect(() => {
-    },[listRoom])
+        socket.on('createRoom',(data) => {
+            setListRoom([...listRoom, data])
+        })
+        console.log(listRoom)
+    },[socket])
 
 
     return (
       <div className="container mx-auto bg-stone-500 rounded-sm px-4 py-6 flex justify-around">
 
-          <div className="w-2/6 text-white px-3 py-4 rounded-xl bg-stone-600">
-              <h2 className='text-center mb-5 font-bold text-3xl'>Buat Room Baru</h2>
-            <input type="text" className='w-full bg-transparent border-white border-b mb-5 text-xl text-white outline-none py-4' placeholder='nama room' value={roomName} onChange={(e) => {setRoomName(e.target.value)}} />
-            <select className='w-full mb-5 py-1 bg-stone-500 rounded-xl outline-none' onChange={(e) => {setRoomStat(e.target.value)}}>
-                <option className='w-full mb-5 text-center bg-stone-500 rounded-xl' value="private">Private</option>
-                <option className='w-full mb-5 text-center bg-stone-500 rounded-xl' value="public">public</option>
-            </select>
-                <button onClick={createRoom} className='text-base rounded-xl py-1 text-center bg-stone-500 mb-5 w-full'>Create room</button>    
-          </div>
+        <Send />
 
           <div className="w-3/6 bg-stone-600 rounded-xl py-6 px-4 ">
                 <h2 className="text-3xl font-bold text-center mb-4 text-white">Daftar Room</h2>
@@ -37,14 +38,14 @@ const App = () => {
                         <p className="text-base font-bold">id room</p>
                         <p className="text-base font-bold">player</p>
                     </div>
-                <div className="flex h-60 flex-col bg-stone-500 px-4 py-6 overflow-y-auto">
+                <div className="flex h-60 flex-col bg-stone-500 px-4 py-6 overflow-y-scroll">
                   
                 {
                     listRoom?.map((data) => (
                         <div className="flex mb-5 px-2 rounded-md py-2 bg-stone-600 text-white justify-between cursor-pointer">
-                        <p className="text-base font-bold">{data.ruangan}</p>
-                        <p className="text-base font-bold">{data.id}</p>
-                        <p className="text-base font-bold">{data.player}</p>
+                        <p className="text-base font-bold">{data.roomName}</p>
+                        <p className="text-base font-bold">{data.roomId}</p>
+                        <p className="text-base font-bold">{data.players}</p>
                     </div>
                     ))
                 }
---
+++ src/Send.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useState } from 'react/cjs/react.development'
+import io  from 'socket.io-client'
+
+const Send = () => {
+    let [roomName,setRoomName] = useState()
+    let [roomStat,setRoomStat ] = useState()
+    let [listRoom,setListRoom] = useState([])
+
+    const socket = io.connect('http://localhost:3001/web-socket/home',{
+    })
+    let createRoom = () => {
+        setRoomName('')
+        socket.emit('createRoom',{roomType: "public",roomName: roomName,players: "tes"})
+     
+    }
+
+  
+    return (
+          <div className="w-2/6 text-white px-3 py-4 rounded-xl bg-stone-600">
+              <h2 className='text-center mb-5 font-bold text-3xl'>Buat Room Baru</h2>
+            <input type="text" className='w-full bg-transparent border-white border-b mb-5 text-xl text-white outline-none py-4' placeholder='nama room' value={roomName} onChange={(e) => {setRoomName(e.target.value)}} />
+            <select className='w-full mb-5 py-1 bg-stone-500 rounded-xl outline-none' onChange={(e) => {setRoomStat(e.target.value)}}>
+                <option className='w-full mb-5 text-center bg-stone-500 rounded-xl' value="private">Private</option>
+                <option className='w-full mb-5 text-center bg-stone-500 rounded-xl' value="public">public</option>
+            </select>
+                <button onClick={createRoom} className='text-base rounded-xl py-1 text-center bg-stone-500 mb-5 w-full'>Create room</button>    
+          </div>
+    )
+}
+
+export default Send
